ffc666142c20eea6d3c47d162560ccda37b3e09f
Regan Nguyen <regankelowna@gmail.com>
2023-03-22 14:33:19 -0700

    added the project function

---
+++ analysis/code/project_functions3.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+def load_and_process(url_or_path_to_csv_file): 
+    df1 = (
+    pd.read_csv(url_or_path_to_csv_file)
+    )
+    
+    df2 = (
+        df1
+        .groupby('season')
+        .apply(lambda x: x.nlargest(10, 'pts')
+        .mean(numeric_only=True))
+        .reset_index()[['season', 'pts']] 
+    )
+    
+    return df2
