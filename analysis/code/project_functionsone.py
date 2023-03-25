4839c70cc7dfda2c24bef0cbd322f900c86d37a6
A-Delfs <adamdelfs@icloud.com>
2023-03-25 14:17:52 -0700

    all done analysis one for  milestone 4

--- analysis/code/project_functionsone.py
+++ analysis/code/project_functionsone.py
@@ -1,24 +1,23 @@
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-def load_and_process(url_or_path_to_csv_file):
-    
-    df1 = (
+
+def load_process(url_or_path_to_csv_file):
+    df = (
         pd.read_csv(url_or_path_to_csv_file)
-        .drop(columns=[col for col in df.columns if col not in ["ts_pct", "pts", "player_name", "season"]])
+        .drop(columns=[col for col in pd.read_csv(url_or_path_to_csv_file).columns if col not in ["ts_pct", "pts", "player_name", "season", "gp"]])
         .rename(columns={"ts_pct": "overall_pct"})
         .dropna()
     )
 
-    
     players = ["Stephen Curry", "Giannis Antetokounmpo", "Luka Doncic", "Kevin Durant", "Nikola Jokic", "Joel Embiid", "LeBron James",'James Harden', 'Trae Young','Jayson Tatum']
     player_stats = (
-        df1[df1["player_name"].isin(players)][["player_name", "pts", "gp"]]
+        df[df["player_name"].isin(players)][["player_name", "pts", "gp"]]
         .assign(true_total_pts=lambda x: x["pts"] * x["gp"])
         .groupby("player_name")
-        .agg({"true_total_pts": "sum"})
+        .agg({"true_total_pts": "sum", "gp": "sum"})
         .reset_index()
     )
 
     return player_stats
-
